a2e6c1deef639027c1c9a74534e6fff4d2757cd8
septor <patrickweaver@gmail.com>
2012-04-07 21:02:39 -0400

    Italian language file added

---
+++ languages/Italian.php
@@ -0,0 +1,137 @@
+﻿<?php
+
+define("LAN_SUR1","Vedi risposte");
+define("LAN_SUR2","Ti è consentito compilare questo modulo una sola volta e i nostri records mostrano che hai già inviato il sondaggio.");
+define("LAN_SUR3","Campo richiesto");
+define("LAN_SUR4","* Campi obbligatori");
+define("LAN_SUR5","Invia");
+define("LAN_SUR6","Non hai i permessi per compilare questo sondaggio.");
+define("LAN_SUR7","Modulo inviato:");
+define("LAN_SUR8","Grazie per aver risposto al nostro Sondaggio.");
+define("LAN_SUR9","Grazie");
+define("LAN_SUR10","Risposta");
+
+define("LAN_SUR11","Cerca");
+define("LAN_SUR12","Elenco risultati");
+define("LAN_SUR13","Vedi statistiche risposte");
+define("LAN_SUR14","Numero di risposte");
+define("LAN_SUR15","Nessun risultato");
+define("LAN_SUR16","Domanda");
+define("LAN_SUR17","Risultati");
+define("LAN_SUR18","Risultati del sondaggio");
+define("LAN_SUR19","Prima risposta");
+define("LAN_SUR20","Ultima risposta");
+define("LAN_SUR21","Campo Testo");
+define("LAN_SUR22","Campo Area di testo");
+define("LAN_SUR23","Campo Nome");
+define("LAN_SUR24","Campo Data");
+define("LAN_SUR25","di");
+define("LAN_SUR26","Edita Risultati");
+define("LAN_SUR27","Aggiorna Risultati");
+
+define("LAN_SUR28","Stai per cancellare un record da");
+define("LAN_SUR29","Conferma cancellazione");
+define("LAN_SUR30","Cancella");
+define("LAN_SUR31","Risultato cancellato");
+define("LAN_SUR32","Cancellazione non avvenuta");
+define("LAN_SUR33","Vuoi stampare questa pagina?");
+define("LAN_SUR34","Stampa");
+define("LAN_SUR35","Printer friendly view");
+define("LAN_SUR36","Edita");
+define("LAN_SUR37","Cerca Testo");
+define("LAN_SUR38","Record");
+define("LAN_SUR39","Totale Records");
+define("LAN_SUR40","Email non valido!");
+define("LAN_SUR41","Deve essere un numero valido.");
+define("LAN_SUR42","Sondaggio inviato");
+
+define("LAN_SUR99","Cancella risultato");
+define("LAN_SUR100","Esporta i risultati in Microsoft Word");
+define("LAN_SUR101","Esporta i risultati in Microsoft Excel");
+
+
+// admin.config.php
+define("ADLAN_SURTYPE_1","testo");
+define("ADLAN_SURTYPE_2","area di testo");
+define("ADLAN_SURTYPE_3","checkbox");
+define("ADLAN_SURTYPE_4","radio");
+define("ADLAN_SURTYPE_5","dropdown");
+define("ADLAN_SURTYPE_6","separatore");
+define("ADLAN_SURTYPE_7","data");
+define("ADLAN_SURTYPE_8","nome");
+define("ADLAN_SURTYPE_9","calcolo");
+define("ADLAN_SURTYPE_10","email");
+define("ADLAN_SURTYPE_11","numero");
+define("ADLAN_SURTYPE_12","email a");
+
+define("ADLAN_SUR2","Sondaggio e tutti i risultati cancellati con successo.");
+define("ADLAN_SUR3","Seleziona il box di conferma per cancellare il sondaggio");
+define("ADLAN_SUR4","Campi lasciati vuoti");
+define("ADLAN_SUR5","Nessun sondaggio presente");
+define("ADLAN_SUR6","Edita");
+define("ADLAN_SUR7","Crea nuovo sondaggio");
+define("ADLAN_SUR8","Cancella");
+define("ADLAN_SUR9","Sondaggi esistenti");
+define("ADLAN_SUR10","Nome:");
+define("ADLAN_SUR11","Gruppo Utente:");
+define("ADLAN_SUR12","Permetti un solo voto per utente?<br />(non funziona se aperto a tutti):");
+define("ADLAN_SUR13","Si");
+define("ADLAN_SUR14","No");
+define("ADLAN_SUR15","Invia email a:");
+define("ADLAN_SUR16","Invia risultati al forum:");
+define("ADLAN_SUR17","Salva i risultati per una valutazione statistica:");
+define("ADLAN_SUR18","Risultati visibili a:");
+define("ADLAN_SUR19","Crea Sondaggio - Procedi...");
+define("ADLAN_SUR20","Messaggio del Sondaggio<br /><br />Visualizzato sopra il modulo sondaggio dagli utenti.");
+define("ADLAN_SUR21","Messaggio di Invio Sondaggio<br /><br />Messaggio mostrato all'utente dopo che ha compilato il modulo sondaggio.");
+define("ADLAN_SUR22","Domanda:");
+define("ADLAN_SUR23","R");
+define("ADLAN_SUR24","Tipo");
+define("ADLAN_SUR25","Parametri");
+define("ADLAN_SUR26","Inserisci i valori sottostanti per aggiungere un nuovo campo");
+define("ADLAN_SUR27","Aggiorna informazioni Sondaggio");
+define("ADLAN_SUR28","Link da usare:");
+define("ADLAN_SUR29","seleziona per confermare");
+define("ADLAN_SUR30","-nessuno-");
+define("ADLAN_SUR31","Attenzione: Cambiando l'ordine dei campi verranno cancellati i dati precedenti!");
+define("ADLAN_SUR32","Dato precedente cancellato");
+define("ADLAN_SUR33","Sondaggio aggiornato");
+define("ADLAN_SUR34","H:");
+define("ADLAN_SUR35","Seleziona per creare campo nascosto");
+define("ADLAN_SUR36","Elaborerà qualsiasi operazione matematica su campi numerici.  Sostituisci {x} per ogni campo numerico.  IE  (27+{1})*5");
+define("ADLAN_SUR37","usa dmy per avere Data in formato dd/mm/yyyy, altrimenti di default mm/dd/yyyy.");
+
+define("ADLAN_SUR40","Nome visualizzato nella tabella titolo.");
+define("ADLAN_SUR41","Visibile a:");
+define("ADLAN_SUR42","Chi può visualizzare i dati statistici del sondaggio.");
+define("ADLAN_SUR43","I risultati di ogni Sondaggio verranno inviati per email all'elenco di indirizzi qui.  Separa indirizzi con una virgola.");
+define("ADLAN_SUR44","Salva Risultati:");
+define("ADLAN_SUR45","Vuoi che i risultati vengano salvati nel db per poter produrre dati statistici?");
+define("ADLAN_SUR46","Messaggio Sondaggio:");
+define("ADLAN_SUR47","Messaggio posizionato sopra il Sondaggio per spiegare agli utenti lo scopo del sondaggio, ecc.");
+define("ADLAN_SUR48","Messaggio Invio Sondaggio:");
+define("ADLAN_SUR49","Messaggio mostrato all'utente dopo che il modulo sondaggio è stato compilato.");
+define("ADLAN_SUR50","Domande Sondaggio");
+define("ADLAN_SUR51","Domanda.");
+define("ADLAN_SUR52","R:");
+define("ADLAN_SUR53","Seleziona per rendere il campo obbligatorio.");
+define("ADLAN_SUR54","Che tipo di risposta verrà richiesta all'utente.<br /><br />L'eccezione è il tipo 'separatore'.  Questo viene usato per aggiungere del testo tra i campi. Per esempio per una separazione di domanda per categoria. Il capo domanda supporta testo con bbcode.  Puoi usare il parametro del 'menu' per racchiudere in una tabella stile menu.");
+define("ADLAN_SUR55","Tutti i parametri dovranno essere separati da virgole, quindi i parametri saranno:");
+define("ADLAN_SUR56","Testo:");
+define("ADLAN_SUR57","lunghezza dell'area di testo, lunghezza max. consentita");
+define("ADLAN_SUR58","area di testo:");
+define("ADLAN_SUR59"," # di colonne, # di righe");
+define("ADLAN_SUR60","checkbox:");
+define("ADLAN_SUR61","Opzioni separate da virgole");
+define("ADLAN_SUR62","radio:");
+define("ADLAN_SUR63","dropdown:");
+define("ADLAN_SUR65","Help Sondaggio");
+define("ADLAN_SUR66","Campo");
+define("ADLAN_SUR67","Impostazioni");
+define("ADLAN_SUR68","Chi può editare i risultati");
+define("ADLAN_SUR69","Clicca per mostrare configurazione principale del Sondaggio");
+define("ADLAN_SUR70","Crea copia di sondaggio esistente");
+define("ADLAN_SUR71","Crea");
+define("ADLAN_SUR72","Sondaggio creato");
+
+?>
