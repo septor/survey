37b70c5bd508eef4fb405ea5301f6c9a427d6210
septor <patrickweaver@gmail.com>
2013-05-05 08:53:00 -0400

    v2 notices, cleanup

--- e_meta.php
+++ e_meta.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('e107_INIT')) { exit(); }
-echo "<script type='text/javascript' src='".e_PLUGIN."survey/survey_cal.js'></script>";
-echo "<script type='text/javascript' src='".e_PLUGIN."survey/survey.js'></script>";
-echo "<link rel='stylesheet' href='".e_PLUGIN."survey/survey_cal.css' />";
+echo "<script type='text/javascript' src='".e_PLUGIN."survey/survey_cal.js'></script>
+<script type='text/javascript' src='".e_PLUGIN."survey/survey.js'></script>
+<link rel='stylesheet' href='".e_PLUGIN."survey/survey_cal.css' />";
 ?>
