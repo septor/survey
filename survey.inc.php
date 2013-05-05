2a489be75f2d93466212acec8bc71291547c9cfa
septor <patrickweaver@gmail.com>
2013-05-05 11:38:20 -0400

    code format cleanup

--- survey.inc.php
+++ survey.inc.php
@@ -1,17 +1,9 @@
 <?php
-/*
-+---------------------------------------------------------------+
-|	e107 website system
-|	survey.inc.php
-|
-|	Released under the terms and conditions of the
-|	GNU General Public License (http://gnu.org).
-+---------------------------------------------------------------+
-*/
 require_once(e_HANDLER."form_handler.php");
+
 class myform extends form
 {
-	function form_select($form_name,$form_options,$form_value)
+	function form_select($form_name, $form_options, $form_value)
 	{
 		$ret = "\n<select name='".$form_name."' class='tbox'>";
 		foreach($form_options as $o)
@@ -46,103 +38,123 @@ function show_form_field($field)
 	switch($field['field_type'])
 	{
 		case 1:  // text
-		list($size,$maxlength)=explode(",",$field['field_choices']);
-		$ret .= $frm -> form_text("results[{$fn}]",$size,$_res[$fn],$maxlength);
+		list($size, $maxlength) = explode(",", $field['field_choices']);
+		$ret .= $frm->form_text("results[".$fn."]", $size, $_res[$fn], $maxlength);
 		break;
+
 		case 2: // textarea
-		list($cols,$rows)=explode(",",$field['field_choices']);
-		$ret .= $frm -> form_textarea("results[{$fn}]",$cols,$rows,$_res[$fn]);
+		list($cols, $rows) = explode(",", $field['field_choices']);
+		$ret .= $frm->form_textarea("results[".$fn."]", $cols, $rows, $_res[$fn]);
 		break;
+
 		case 3:  //checkbox
-		$options=explode(",",$field['field_choices']);
-		$checked_vals=unserialize($_res[$fn]);
-		foreach($checked_vals as $k => $v){
-			$checked_vals[$k]=trim($v);
+		$options = explode(",", $field['field_choices']);
+		$checked_vals = unserialize($_res[$fn]);
+		foreach($checked_vals as $k => $v)
+		{
+			$checked_vals[$k] = trim($v);
 		}
-		foreach($options as $o){
-			$ch = (in_array(trim($o),$checked_vals)) ? 1 : 0;
-			$ret .= "<div style='vertical-align:top'>".$frm -> form_checkbox("results[{$fn}][]",$o,$ch)."&nbsp;{$o}</div>";
+		foreach($options as $o)
+		{
+			$ch = (in_array(trim($o), $checked_vals)) ? 1 : 0;
+			$ret .= "<div style='vertical-align:top'>".$frm->form_checkbox("results[".$fn."][]", $o, $ch)."&nbsp;".$o."</div>";
 		}
 		break;
+
 		case 4:  //radio
-		$options=explode(",",$field['field_choices']);
-		foreach($options as $o){
+		$options = explode(",", $field['field_choices']);
+		foreach($options as $o)
+		{
 			$ch = ($_res[$fn] == $o) ? 1 : 0;
-			$ret .= $frm -> form_radio("results[{$fn}]",$o,$ch)."&nbsp;{$o}<br />";
+			$ret .= $frm->form_radio("results[".$fn."]", $o, $ch)."&nbsp;".$o."<br />";
 		}
 		break;
+
 		case 5:  //dropdown
-		$options=explode(",",$field['field_choices']);
-		$o=array();
-		$o[0]="---";
-		foreach($options as $x){
-			$o[]=trim($x);
+		$options = explode(",", $field['field_choices']);
+		$o = array();
+		$o[0] = "---";
+		foreach($options as $x)
+		{
+			$o[] = trim($x);
 		}
-		$ret .= $frm -> form_select("results[{$fn}]",$o,$_res[$fn]);
+		$ret .= $frm->form_select("results[".$fn."]", $o, $_res[$fn]);
 		break;
+
 		case 6:  //separator
-		$options=explode(",",$field['field_choices']);
-		if($options[0] == "menu"){
-			$oldstyle=$style;
-			if($options[1] != ""){$style=$options[1];}
+		$options = explode(",", $field['field_choices']);
+		if($options[0] == "menu")
+		{
+			$oldstyle = $style;
+			if($options[1] != ""){ $style = $options[1]; }
 			ob_end_flush();
 			ob_start();
-			$ns -> tablerender($tp->toHTML($field['field_text']),"");
+			$ns->tablerender($tp->toHTML($field['field_text']), "");
 			$ret .= ob_get_contents();
 			ob_end_clean();
-			$style=$oldstyle;
-			} else {
-				$ret .= $tp->toHTML($field['field_text']);
-			}
-			break;
-			case 7:  //date
-			if($field['field_choices'] == "dmy"){
-				$fmt="d/m/Y";
-				$calfmt="dd/mm/y";
-				$calmsg="dd/mm/yyyy";
-				} else {
-					$fmt="m/d/Y";
-					$calfmt="mm/dd/y";
-					$calmsg="mm/dd/yyyy";
-				}
-				if($_res[$fn]){
-					$xdate=$_res[$fn];
-				}
-				$ret .= "
-				<input class='tbox' type='text' name='results[{$fn}]' id='date_{$fn}' value='$xdate' />
-				<input class='tbox' type='button' name='reset' value=' ... ' id='trigger_{$fn}' /> {$calmsg}
-				<script type='text/javascript'>
-				Calendar.setup({
-				inputField     :    'date_{$fn}',
-				ifFormat       :    '{$calfmt}',
-				button         :    'trigger_{$fn}',
-				singleClick    :    true
-				});
-				</script>
-				";
-				break;
-				case 8:  //name
-				if($survey_class){
-					$ret .= $frm -> form_hidden("results[{$fn}]",USERNAME).USERNAME;
-					} else {
-						$ret .= $frm -> form_text("results[{$fn}]",$size,$_res[$fn],$maxlength);
-					}
-					break;
-					case 10:  //email
-					list($size,$maxlength)=explode(",",$field['field_choices']);
-					//if($survey_class){
-						//$ret .= $frm -> form_hidden("results[{$fn}]",USEREMAIL).USEREMAIL;
-						//} else {
-							$ret .= $frm -> form_text("results[{$fn}]",$size,$_res[$fn],$maxlength);
-						//}
-						break;
-						case 11:  // number
-						case 12:	 //emailto
-						list($size,$maxlength)=explode(",",$field['field_choices']);
-						$ret .= $frm -> form_text("results[{$fn}]",$size,$_res[$fn],$maxlength);
-						break;
-					}
-					return $ret;
-				}
+			$style = $oldstyle;
+		}
+		else
+		{
+			$ret .= $tp->toHTML($field['field_text']);
+		}
+		break;
 
-				?>
+		case 7:  //date
+		if($field['field_choices'] == "dmy")
+		{
+			$fmt = "d/m/Y";
+			$calfmt = "dd/mm/y";
+			$calmsg = "dd/mm/yyyy";
+		}
+		else
+		{
+				$fmt = "m/d/Y";
+				$calfmt = "mm/dd/y";
+				$calmsg = "mm/dd/yyyy";
+		}
+		if($_res[$fn])
+		{
+			$xdate = $_res[$fn];
+		}
+		$ret .= "
+		<input class='tbox' type='text' name='results[".$fn."]' id='date_".$fn."' value='".$xdate."' />
+		<input class='tbox' type='button' name='reset' value=' ... ' id='trigger_".$fn."' /> ".$calmsg."
+		<script type='text/javascript'>
+			Calendar.setup({
+			inputField     :    'date_".$fn."',
+			ifFormat       :    '".$calfmt."',
+			button         :    'trigger_".$fn."',
+			singleClick    :    true
+			});
+		</script>
+		";
+		break;
+
+		case 8:  //name
+		if($survey_class)
+		{
+			$ret .= $frm->form_hidden("results[".$fn."]", USERNAME).USERNAME;
+		}
+		else
+		{
+			$ret .= $frm->form_text("results[".$fn."]", $size, $_res[$fn], $maxlength);
+		}
+		break;
+
+		case 10:  //email
+		list($size, $maxlength) = explode(",", $field['field_choices']);
+		$ret .= $frm->form_text("results[".$fn."]", $size, $_res[$fn], $maxlength);
+		break;
+
+		case 11:  // number
+
+		case 12:	 //emailto
+		list($size, $maxlength) = explode(",", $field['field_choices']);
+		$ret .= $frm->form_text("results[".$fn."]", $size, $_res[$fn], $maxlength);
+		break;
+	}
+	return $ret;
+}
+
+?>
